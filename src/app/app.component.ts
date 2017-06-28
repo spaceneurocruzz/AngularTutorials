6988783218e730533099d8dc0eb323a7a695d05f
Sandy Lin <hjsandy_lin@acbel.com>
2017-06-28 19:17:42 +0800

    learn components and templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>Hey guys!</h1>
+  <p> how are u?</p>
+  <p>{{myObject.location}}</p>
+    <ul>
+      <li *ngFor="let arr of myArr">{{arr}}</li>
+      <li *ngIf="myArr; else otherTmpl">yeah</li>
+    </ul>
+    <div *ngIf="myArr; then Tmpl1 else Tmpl2">yeah</div>
+    <ng-template #otherTmpl>No</ng-template>
+    <ng-template #Tmpl1>truth</ng-template>
+    <ng-template #Tmpl2>False</ng-template>
+    `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  myObject = {
+    gender: 'male',
+    age:33,
+    location: 'USA'
+  }
+  //myArr = ['123','321','555']
+  nyArr=false;
 }
