9694393d1b2ac193cf4fa46eb122ab9a1c7205dd
Sandy Lin <hjsandy_lin@acbel.com>
2017-06-29 21:58:21 +0800

    left lessons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
+import { trigger,state,style,transition,animate,keyframes} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -14,8 +16,66 @@ import { Component } from '@angular/core';
     <ng-template #otherTmpl>No</ng-template>
     <ng-template #Tmpl1>truth</ng-template>
     <ng-template #Tmpl2>False</ng-template>
+
+    <img src="{{angularLogo}}">
+    <img [src]="angularLogo">
+    <img bind-src="angularLogo">
+    <button [disabled]="buttonStatus">My button1</button>
+
+    <button (click)="myEvent($event)">My button2 </button>
+    <button (mouseenter)="myEvent($event)">My button3 </button>
+    
+    <h1>Hello!</h1>
+    <h1 [class]="titleClass">Hello!</h1>
+    <h1 [class.red-title]="titleClass2">Hello!!</h1>
+    <h1 [ngClass]="titleClasses">Hello!!!</h1>
+    
+    <h1 [style.color]="titleStyle">Hello!!!!</h1>
+    <h1 [style.color]="titleStyles ? 'green':'pink'">Hello!!!!</h1>
+    <h1 [ngStyle]="titleStyless">Hello!!!!</h1>
+
+    <p>{{someProperty}}</p>
+
+    <p [@myAwesomeAnimation]='state' (click)="animateMe()">I will animate</p>
     `,
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles:[`
+    h1{
+        text-decoration:underline;
+    }
+    .red-title {
+      color:red;
+    }
+    .large-title{
+      font-size:4em;
+    } 
+    p{
+      width:200px;
+      background:lightgray;
+      margin:100px auto;
+      text-align:center;
+      padding:20px;
+      font-size:1.5em;
+    }
+    `],
+  animations:[
+    trigger('myAwesomeAnimation',[
+      state('small', style({
+        transform: 'scale(1)',
+      })),
+      state('large', style({
+        transform: 'scale(1.2)',
+      })),
+      
+      transition('small <=>large', animate('300ms ease-in', keyframes([
+        style({opacity: 0, transform: 'translateY(-75%)', offset:0}),
+        style({opacity: 2, transform: 'translateY(35px)', offset:.5}),
+        style({opacity: 1, transform: 'translateY(0', offset:1}),
+      ]))),
+      //style({transform: 'translateY(40px)'
+      //}))),
+    ]),
+   ]
 })
 export class AppComponent {
   myObject = {
@@ -24,5 +84,44 @@ export class AppComponent {
     location: 'USA'
   }
   //myArr = ['123','321','555']
-  nyArr=false;
+  myArr=false;
+
+  angularLogo = 'https://firebase.google.com/images/homepage/build_1x.png'
+  buttonStatus = false;
+  myEvent(event){
+    console.log(event);
+  }
+
+  titleClass = 'red-title';
+  titleClass2 = false;
+  titleClasses = {
+    'red-title':true,
+    'large-title':true
+  }
+
+  titleStyle = 'red';
+  titleStyles = true;
+  titleStyless = {
+    'color':'pink',
+    'font-size':'4em'
+  }
+
+  constructor(private dataService:DataService){
+
+  }
+  someProperty:string = '';
+
+
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log(this.dataService.cars);
+    this.someProperty = this.dataService.myData();
+  }
+
+  state:string='small'
+
+  animateMe(){
+    this.state = (this.state === 'small'?'large':'small');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+import { DataService } from './data.service';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,12 @@ import { MyNewComponentComponent } from './my-new-component/my-new-component.com
     MyNewComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
